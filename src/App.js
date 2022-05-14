5da13c7fac7d905ba56b2736d1bc6c7640b85cc0
YDG <zzmn1234@naver.com>
2022-05-15 00:16:06 +0900

    errormodal 구현

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import react from "react";
+import react,{ useState } from "react";
 import Expense from "./components/expense/Expense";
+import UserList from "./components/expense/UserList";
 
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList)=>{
+      return [...prevUsersList, { name: uName, age: uAge, id: Math.random().toString() }]
+    });
+  }
   return (
     <div>
-      <Expense />
+      <Expense onAddUser={addUserHandler}/>
+      <UserList users={usersList} />
     </div>
   );
 }
--- src/components/expense/Expense.js
+++ src/components/expense/Expense.js
@@ -3,10 +3,12 @@ import { useState } from "react";
 import styles from "./Expense.module.css";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
+import ErrorModal from "../UI/ErrorModal";
 
-function Input() {
+function Input(props) {
   const [nameInput, setNameInput] = useState("");
   const [ageInput, setAgeInput] = useState("");
+  const [error, setError] = useState();
 
   const nameInputValue = (e) => {
     setNameInput(e.target.value);
@@ -18,16 +20,32 @@ function Input() {
 
   const submitHandler = (e) => {
     e.preventDefault();
-    const expenseData = {
-      name: nameInput,
-      age: ageInput,
-    };
-    console.log(expenseData);
+    if(nameInput.trim().length === 0 || ageInput.trim().length === 0) {
+      setError({
+        title: 'Invalid input',
+        message: 'Please enter a valid name and age (non-empty values).'
+      });
+      return;
+    }
+    if(+ageInput < 1){
+      setError({
+        title: 'Invalid age',
+        message: 'Please enter a valid age (> 0).'
+      });
+      return;
+    }
+    props.onAddUser(nameInput, ageInput);
     setNameInput("");
     setAgeInput("");
   };
 
+  const errorHandler = () => {
+    setError(null)
+  }
+
   return (
+    <div>
+     {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler}/>}
     <Card className={styles.input}>
       <form onSubmit={submitHandler}>
         <label htmlFor="username">Username</label>
@@ -47,6 +65,7 @@ function Input() {
         <Button type="submit">Add User</Button>
       </form>
     </Card>
+    </div>
   );
 }
 export default Input;
